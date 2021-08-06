046bc78c83f08075ce2abddf25551e2343da9ba8
Fernando Nevarez <fnevar623@west-mec.local>
2021-08-05 18:25:08 -0700

    updated code

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,14 +7,16 @@ const colVaule = 8
 const finalRowValue;
 const finalColVaule;
 
-const rowRange = []
+const rowRange = [0, 128]
+const colRange = [0, 8]
 
 let newArray = input.forEach((spot) => {
     const spotArray = spot.split("")
     for(let i = 0; i < spotArray.length; i++){
         if(spotArray[i] === 'F'){
-            finalRowValue = rowValue / 2
+            // make it so that it updated the row range to the new range
             
         }
+        
     }
 })
