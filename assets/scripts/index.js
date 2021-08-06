3d15097d1fb7bb0f2053ed2b654058e4ac27b5b5
Fernando Nevarez <fnevar623@west-mec.local>
2021-08-05 18:20:53 -0700

    added the stuff i worked on in class

---
+++ assets/scripts/index.js
@@ -0,0 +1,20 @@
+console.log(input.length)
+
+// Qustion One
+const rowtValue = 128
+const colVaule = 8
+
+const finalRowValue;
+const finalColVaule;
+
+const rowRange = []
+
+let newArray = input.forEach((spot) => {
+    const spotArray = spot.split("")
+    for(let i = 0; i < spotArray.length; i++){
+        if(spotArray[i] === 'F'){
+            finalRowValue = rowValue / 2
+            
+        }
+    }
+})
